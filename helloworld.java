7207ee22951f0e5df80d84e4f5bf9c74a3468dba
lsn <lsn@qq.com>
2021-09-05 21:08:23 +0800

    first commit

---
+++ helloworld.java
@@ -0,0 +1,26 @@
+public class HelloWord{
+public class HelloWord{
+public class HelloWord{
+public class HelloWord{
+public class HelloWord{
+public class HelloWord{
+public class HelloWord{
+public class HelloWord{
+	public static void main(String[] args){
+		System.out.println("1hello word");
+		System.out.println("2hello word");
+		System.out.println("3hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+		System.out.println("4hello word");
+	}
+}
